90d74f47baede8fa10fa5b21470f2bfc6a0bb472
dsthedev <10052665+dsthedev@users.noreply.github.com>
2023-03-16 00:55:23 -0500

    lol

--- web/src/pages/IsitadropPage/IsitadropPage.tsx
+++ web/src/pages/IsitadropPage/IsitadropPage.tsx
@@ -20,6 +20,24 @@ const IsitadropPage = () => {
         <hr />
         <IsItADrop></IsItADrop>
         <hr />
+        <div className="box">
+          <p>
+            <strong>What&apos;s a &quot;drop&quot;?</strong>
+            <span>
+              A <em>drop</em> is a room or area where carpet is going to be
+              installed that doesn&apos;t need 2 pieces to be seamed together.
+              Typically, this is a small room no wider than the width of a roll
+              of carpet; usually 12 or 15 ft wide.
+            </span>
+          </p>
+          <br />
+          <p>
+            This calculator caps the minimum width to be 1&quot; less than the
+            roll width, just in case the original measurement is a little
+            short...
+          </p>
+          <p className="has-text-centered is-size-1">&#129300;</p>
+        </div>
       </main>
 
       <CalcFooter></CalcFooter>
